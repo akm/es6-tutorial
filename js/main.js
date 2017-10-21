d4a353ff27054260875721436317cb82699ac083
akm <akm2000@gmail.com>
2017-10-21 17:30:53 +0900

    Return object instead of monthlyPayment from calculateMonthlyPayment

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ let calculateMonthlyPayment = function (principal, years, rate) {
         monthlyRate = rate / 100 / 12;
     }
     let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
-    return monthlyPayment;
+  return {principal, years, rate, monthlyPayment, monthlyRate};
 };
 
 document.getElementById('calcBtn').addEventListener('click', function () {
